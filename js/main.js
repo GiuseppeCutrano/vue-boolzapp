debf87341401224e8669b8f882d76189186e1e68
GiuseppeCutrano <giuseppecutrano.info@gmail.com>
2020-11-20 15:47:46 +0100

    add sezione user + sezione notifiche. iniziato top header

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 const myApp = new Vue({
   el: "#root",
   data: {
-
+    // Nostro account
+    user: {
+      name: 'Nome Utente',
+      avatar: 'Propio avatar'
+    },
+     // contatti
+     contacts:[
+       {
+         name:'Michele',
+         avatar:'avatar_1'
+       }
+     ]
 
   },
 
