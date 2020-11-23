6a06b2bc108aacc05dd51ba5f6a4a6b5caa84555
GiuseppeCutrano <giuseppecutrano.info@gmail.com>
2020-11-23 16:06:12 +0100

    aggiunto footer modifiche js

--- js/main.js
+++ js/main.js
@@ -12,9 +12,27 @@ const myApp = new Vue({
          name:'Michele',
          avatar:'_1',
          visible: true,
-         messagges:[{
-           // qui vanno i messaggi
-         }]
+         messagges:[
+           {
+           date: '10/01/2020 15:30:55',
+           message: 'Hai portato a spasso il cane?',
+           status: 'sent'
+         },
+
+         { date: '10/01/2020 15:50:00',
+           message: 'Ricordati di dargli da mangiare',
+           status: 'sent'
+
+         },
+         {
+            date: '10/01/2020 16:15:22',
+            message: 'Tutto fatto!',
+            status: 'received'
+          }
+
+
+       ],
+
        },
        {
          name:'Fabio',
@@ -43,7 +61,7 @@ const myApp = new Vue({
 
        }
      ]
-
+      
   },
 
 })
